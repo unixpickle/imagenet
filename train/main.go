36f3c15bf0eb41368836c5d6fe7ece71e8bb2031
Alex Nichol <unixpickle@gmail.com>
2017-01-25 11:01:56 -0500

    use flags for train command

--- train/main.go
+++ train/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"math/rand"
@@ -16,36 +17,43 @@ import (
 	"github.com/unixpickle/serializer"
 )
 
-const (
-	ImageDirArg = 1
-	OutNetArg   = 2
-
-	StepSize       = 1e-4
-	BatchSize      = 12
-	ValidationSize = 0.1
-
-	LogInterval = 4
-)
-
 func main() {
 	rand.Seed(time.Now().UnixNano())
 
-	if len(os.Args) != 3 {
-		fmt.Fprintln(os.Stderr, "Usage:", os.Args[0], "image_dir out_net")
+	var imageDir string
+	var outNet string
+	var stepSize float64
+	var batchSize int
+	var validationSize float64
+	var logInterval int
+
+	flag.StringVar(&imageDir, "samples", "", "sample directory")
+	flag.StringVar(&outNet, "out", "out_net", "network file")
+	flag.Float64Var(&stepSize, "step", 0.001, "step size")
+	flag.IntVar(&batchSize, "batch", 12, "batch size")
+	flag.Float64Var(&validationSize, "validation", 0.1, "validation fraction")
+	flag.IntVar(&logInterval, "logint", 4, "validation log interval")
+
+	flag.Parse()
+
+	if imageDir == "" || outNet == "" {
+		fmt.Fprintln(os.Stderr, "Required flags: -samples and -out")
+		fmt.Fprintln(os.Stderr)
+		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
 	log.Println("Loading samples...")
-	samples, err := imagenet.NewSampleList(os.Args[ImageDirArg])
+	samples, err := imagenet.NewSampleList(imageDir)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to read sample listing:", err)
 		os.Exit(1)
 	}
-	validation, training := anysgd.HashSplit(samples, ValidationSize)
+	validation, training := anysgd.HashSplit(samples, validationSize)
 	log.Println("Loaded", validation.Len(), "validation,", training.Len(), "training.")
 
 	log.Println("Loading/creating network...")
-	network, err := LoadOrCreateNetwork(os.Args[OutNetArg], samples)
+	network, err := LoadOrCreateNetwork(outNet, samples)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to create network:", err)
 		os.Exit(1)
@@ -64,26 +72,26 @@ func main() {
 		Gradienter:  t,
 		Transformer: &anysgd.Adam{},
 		Samples:     samples,
-		Rater:       anysgd.ConstRater(0.001),
+		Rater:       anysgd.ConstRater(stepSize),
 		StatusFunc: func(b anysgd.Batch) {
-			if iterNum%LogInterval != 1 {
+			if iterNum%logInterval != 1 {
 				log.Printf("iter %d: cost=%v", iterNum, t.LastCost)
 			} else {
 				anysgd.Shuffle(validation)
-				valid := validation.Slice(0, BatchSize)
+				valid := validation.Slice(0, batchSize)
 				batch, _ := t.Fetch(valid)
 				vCost := anyvec.Sum(t.TotalCost(batch.(*anyff.Batch)).Output())
 				log.Printf("iter %d: cost=%v validation=%v", iterNum, vCost, t.LastCost)
 			}
 			iterNum++
 		},
-		BatchSize: BatchSize,
+		BatchSize: batchSize,
 	}
 
 	log.Println("Press ctrl+c once to stop...")
 	s.Run(rip.NewRIP().Chan())
 
-	if err := serializer.SaveAny(os.Args[OutNetArg], network); err != nil {
+	if err := serializer.SaveAny(outNet, network); err != nil {
 		fmt.Fprintln(os.Stderr, "Failed to save network:", err)
 		os.Exit(1)
 	}
